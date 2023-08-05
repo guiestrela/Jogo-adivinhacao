f01f69ae6a6d64c7de19961d4d3b1ac44f642389
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-08-05 16:11:09 -0300

    Update adivinhacao.py

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,3 +1,9 @@
+print("*********************************")
 print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
 
+numero_secreto = 43
 
+chute = input("Digite o seu numero: ")
+
+print(f"Voçe digitou {chute}")

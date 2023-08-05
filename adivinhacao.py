34a01b3d770cef6126d5c95d0bcb784e462f58e3
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-08-05 16:25:04 -0300

    continuando o codigo

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,6 +4,13 @@ print("*********************************")
 
 numero_secreto = 43
 
-chute = input("Digite o seu numero: ")
+chute = int(input("Digite o seu numero: "))
 
 print(f"Voçe digitou {chute}")
+
+if (numero_secreto == chute):
+    print("você ecertou")
+else:
+    print("você errou!!!")
+
+print("Fim do jogo")

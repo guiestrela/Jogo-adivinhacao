943915fc7f466373b52d995c8ef99a038896438e
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-08-06 22:53:05 -0300

    quase finalizado

---
+++ forca.py
@@ -0,0 +1,7 @@
+def jogar():
+    print("********************************")
+    print("***Bem vindo no jogo da Forca***")
+    print("********************************")
+
+
+    print("Fim do Jogo!!!")
